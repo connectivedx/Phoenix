58bc16824e6683f021d823d0bd4dd941bce8ecf2
Alex Mueller <ajmueller6@gmail.com>
2014-01-21 18:32:32 -0800

    Moving patterns to their own file.  Continued polish of the responsive tabs example.

---
+++ pattern-primer.php
@@ -0,0 +1,264 @@
+<html lang="en" class="js"><head>
+	<title>Pattern Primer</title>
+	<meta charset="UTF-8">
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="robots" content="index, follow" />
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="ISITE Design">
+	<meta name="apple-mobile-web-app-title" content="CUSTOM SHORT NAME">
+	
+	<!-- icon and tile color in hex # for Windows phones -->
+	<meta name="msapplication-TileImage" content="/path/to/touch-icon.png">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	
+	<link rel="stylesheet" media="all" href="_resources/css/style.css">
+	<script type="text/javascript" src="_resources/js/modernizr.js"></script>
+	<script type="text/javascript" src="_resources/js/respond.js"></script>
+		
+	<!-- For everything else --> 
+	<link rel="shortcut icon" href="/path/to/shortcut-icon.png"> 
+	<!-- Serve one icon for all Apple devices -->
+	<link rel="apple-touch-icon-precomposed" href="/path/to/touch-icon.png"> 
+</head>
+
+<body class="">
+	<section class="wrapper">
+		<div class="row">
+			<h1>Level 1 heading</h1>
+			<h2>Leven 2 heading</h2>
+			<h3>Level 3 heading</h3>
+			<h4>Level 4 heading</h4>
+			<p>
+				This is a paragraph of text. Some of the text may be <em>emphasised</em> and some it might even be <strong>strongly emphasised</strong>. Occasionally <q>quoted text</q> may be found within a paragraph …and of course <a href="#">a link</a> may appear at any point in the text. The average paragraph contains five or six sentences although some may contain as little or one or two while others carry on for anything up to ten sentences and beyond.
+			</p>
+		</div>
+
+		<div class="row divider">
+			<div class="sixteen columns">
+				<h1>Table Style</h1>
+				<table>
+					<thead>
+						<tr>
+							<th>Lorem Ipsum dolor</th>
+							<th>Lorem Ipsum dolor</th>
+							<th>Lorem Ipsum dolor</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Mauris vestibulum auctor</td>
+							<td>Mauris vestibulum auctor</td>
+							<td>Mauris vestibulum auctor</td>
+						</tr>
+						<tr>
+							<td>Mauris vestibulum auctor</td>
+							<td>Mauris vestibulum auctor</td>
+							<td>Mauris vestibulum auctor</td>
+						</tr>
+						<tr>
+							<td>Mauris vestibulum auctor</td>
+							<td>Mauris vestibulum auctor</td>
+							<td>Mauris vestibulum auctor</td>
+						</tr>
+						<tr>
+							<td>Mauris vestibulum auctor</td>
+							<td>Mauris vestibulum auctor</td>
+							<td>Mauris vestibulum auctor</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+
+		<div class="row divider">
+			<div class="eight columns">
+				<h1>Lists</h1>
+				<h2>Unordered List</h2>
+				<ul>
+					<li>Short Item in the list</li>
+					<li>Short Item in the list</li>
+					<li>Short Item in the list</li>
+				</ul>
+				<h2>Unordered List with Links</h2>
+				<ul class="links">
+					<li><a href="#">Short Item in the list</a></li>
+					<li><a href="#">Short Item in the list</a></li>
+					<li><a href="#">Short Item in the list</a></li>
+				</ul>
+				<h2>Unordered List with Arrows</h2>
+				<ul class="list-arrow">
+					<li>Short Item in the list</li>
+					<li>Short Item in the list</li>
+					<li>Short Item in the list</li>
+					<li>
+						Really long item in the list, Really long item in the list, Really long item in the list, Really long item in the list, Really long item in the list, Really long item in the list, Really long item in the listReally long item in the list, Really long item in the list, Really long item in the list, Really long item in the list, Really long item in the list, Really long item in the list
+					</li>
+				</ul>
+				<h2>Ordered List:</h2>
+				<ol>
+					<li>Short Item in the list</li>
+					<li>Short Item in the list</li>
+					<li>Short Item in the list</li>
+				</ol>
+				<h2>Ordered List with Links:</h2>
+				<ol class="links">
+					<li><a href="#">Short Item in the list</a></li>
+					<li><a href="#">Short Item in the list</a></li>
+					<li><a href="#">Short Item in the list</a></li>
+				</ol>
+			</div>
+
+			<div class="eight columns">
+				<h1>Form Styles</h1>
+				<form action="#">
+					<fieldset>
+						<legend>Text Input</legend>
+						<h2>Text Input</h2>
+						<ol>
+							<li>
+								<label for="testinput">Label for the input</label>
+								<input type="text" id="testinput">
+							</li>
+							<li>
+								<label for="testtextarea">Label for the Textarea</label>
+								<textarea id="testtextarea" cols="30" rows="5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit vestibulum. </textarea>
+							</li>
+							<li>
+						</ol>
+					</fieldset>
+					<fieldset>
+						<legend>Selects, Radios, Checkboxes</legend>
+						<h2>Selects, Radios, Checkboxes</h2>
+						<ol>
+							<li>
+								<label for="testselect">Label for the select</label>
+								<select id="testselect">
+									<option>First</option>
+									<option>Second</option>
+									<option>Third</option>
+								</select>
+							</li>
+							<li>
+							<fieldset>
+								<legend>Select one of the following:</legend>
+								<ol>
+									<li>
+										<label for="options">Label for the radio options</label>
+										<p class="label-secondary">Secondary label for the radio</p>
+										<input type="radio" name="options" id="radio-option-1" value="option 1">
+										<label for="radio-option-1">Option 1</label>
+										<input type="radio" name="options" id="radio-option-2" value="option 2">
+										<label for="radio-option-2">Option 2</label>
+									</li>
+								</ol>
+							</fieldset>
+							</li>
+							<li>
+								<fieldset>
+									<legend>Select at least one of the following:</legend>
+									<ol>
+										<li>
+											<label for="checkbox">Label for the checkbox</label>
+											<p class="label-secondary">Secondary label for the checkbox</p>
+											<input type="checkbox" name="checkbox" id="checkbox-option-1" value="option 1">
+											<label for="checkbox-option-1">Option 1</label>
+											<input type="checkbox" name="checkbox" id="checkbox-option-2" value="option 2">
+											<label for="checkbox-option-2">Option 2</label>
+										</li>
+									</ol>
+								</fieldset>
+							</li>
+							<li>
+								<fieldset>
+								<legend>Inline Checkboxes and Radio Buttons</legend>
+								<ol>
+									<li>
+										<label for="options">Label for the radio options</label>
+										<p class="label-secondary">Secondary label for the radio</p>
+										<input type="radio" name="inline-options" id="inline-radio-option-1" value="option 1">
+										<label for="inline-radio-option-1" class="inline">Option 1</label>
+										<input type="radio" name="inline-options" id="inline-radio-option-2" value="option 2">
+										<label for="inline-radio-option-2" class="inline">Option 2</label>	
+									</li>
+									<li>
+										<label for="checkbox">Label for the checkbox</label>
+										<p class="label-secondary">Secondary label for the checkbox</p>
+										<input type="checkbox" name="inline-checkbox" id="inline-checkbox-option-1" value="option 1">
+										<label for="inline-checkbox-option-1" class="inline">Option 1</label>
+										<input type="checkbox" name="inline-checkbox" id="inline-checkbox-option-2" value="option 2">
+										<label for="inline-checkbox-option-2" class="inline">Option 2</label>
+									</li>
+								</ol>
+								</fieldset>
+							</li>
+						</ol>
+					</fieldset>
+					<fieldset>
+						<legend>Buttons</legend>
+						<h2>Buttons</h2>
+						<ol>
+							<li>
+								<button>Primary button</button>
+								<button class="btn-small">Primary button-small</button>
+							</li>
+							<li>
+								<button class="btn-secondary">Secondary button</button>
+								<button class="btn-secondary btn-small">Secondary button-small</button>
+							</li>
+						</ol>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+
+		<div class="row divider">
+			<div class="sixteen columns">
+				<h1>Responsive Tabs</h1>
+				<div class="responsive-tabs">
+					<h3>Tab 1</h3>
+					<div class="responsive-tabs__panel--active">
+						Tab 1 content
+					</div>
+
+					<h3>Tab 2</h3>
+					<div>Tab 2 content</div>
+
+					<h3>Tab 3</h3>
+					<div>Tab 3 content</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row divider">
+			<div class="sixteen columns">
+				<h1>Fancybox</h1>
+				<ul>
+					<li>
+						<a href="http://www.youtube.com/embed/sVxT0vrHeyM" class="fancybox fancybox.iframe">Youtube (iframe)</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+ 
+	</section><!-- /.wrapper -->
+	
+	<script src="_resources/js/jquery.min.js"></script>
+	<script src="_resources/js/tabs/jquery.responsive-tabs.js"></script>
+	<script src="_resources/js/lightboxes/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript">
+		$(function() {
+			$('.nav-handle').click(function() {
+				$('.nav-list').toggleClass('expanded');
+			});
+			$('select').wrap('<div class="decorator-select"></div>');
+
+			RESPONSIVEUI.responsiveTabs();
+
+			$('.fancybox').fancybox();
+		});
+	</script>
+</body>
+</html>
