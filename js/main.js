74e66f0c8bac0a24977e286c976e577ad2597ca8
Alex Mueller <ajmueller6@gmail.com>
2014-01-28 19:48:50 -0800

    Fixing incorrect reference to sassdoc.json.  Removing documenation-sassdoc folder.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var App = angular.module('FEDTemplateDocApp', []);
 
 App.controller('SCSSCtrl', function($scope, $http) {
-	$http.get('../documentation-sassdoc/sassdoc.json')
+	$http.get('js/sassdoc.json')
 		.then(function(res) {
 			$scope.objects = res.data;
 			console.log(res.data);
