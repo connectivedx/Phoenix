5481cf75453866e5c883538b211b014dbd92c384
Alex Mueller <ajmueller6@gmail.com>
2014-01-28 19:46:45 -0800

    First stab at ISITE branded styling.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+var App = angular.module('FEDTemplateDocApp', []);
+
+App.controller('SCSSCtrl', function($scope, $http) {
+	$http.get('../documentation-sassdoc/sassdoc.json')
+		.then(function(res) {
+			$scope.objects = res.data;
+			console.log(res.data);
+		});
+});
