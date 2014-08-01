11ce4d5caac05a9c77f452b92cf7148c6db60573
Alex Mueller <ajmueller6@gmail.com>
2014-08-01 13:17:12 -0700

    Adding an index page with links to documentation and test pages.  Adding error handling on documentation page.

--- documentation/js/app.js
+++ documentation/js/app.js
@@ -4,5 +4,8 @@ App.controller('SCSSCtrl', function($scope, $http) {
 	$http.get('../../Assets/documentation-sassdoc/sassdoc.json')
 		.then(function(res) {
 			$scope.objects = res.data;
+			$scope.dataLoaded = true;
+		}, function(data) {
+			$scope.dataLoaded = false;
 		});
 });
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	$pageTitle = 'Phoenix by ISITE Design';
+	include_once 'inc/head.php';
+?>
+	<div class="row">
+		<div class="eight columns centered">
+			<img src="/Assets/dist/img/content/logo-placeholder.png" alt="Phoenix by ISITE Design" class="img-align-center">
+		</div>
+	</div>
+	
+	<div class="row">
+		<h1><a href="documentation/">Documentation</a></h1>
+		<h1>Sample Pages</h1>
+		<ul>
+			<li><a href="pattern-primer.php">Pattern Primer</a></li>
+			<li><a href="grid-system-test.php">Grid System Test</a></li>
+			<li><a href="semantic-grid-system-test.php">Semantic Grid System Test</a></li>
+			<li><a href="complex-layout-test">Complex Layout Test</a></li>
+			<li><a href="template-test.php">Template Test</a></li>
+		</ul>
+	</div>
+<?php
+	include_once 'inc/foot.php';
+?>
