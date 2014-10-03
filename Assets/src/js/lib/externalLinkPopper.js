6703550917d30d4417c7a7ed042d0a2a9deab792
Alex Mueller <ajmueller6@gmail.com>
2014-10-03 15:29:22 -0700

    Removing old JavaScript utilities to only leave the cookies library.  These will be expanded on over time as necessary.

--- Assets/src/js/lib/externalLinkPopper.js
+++
@@ -1,8 +0,0 @@
-// @external
-// External link popper
-// checks href against current domain. If not matching and href contains http://, target blank it
-// http://jsfiddle.net/UPhwk/
-
-$('a[href^="http://"]').not('[href*='+window.location.host+']').attr('target','_blank');
-
-// end External link popper
