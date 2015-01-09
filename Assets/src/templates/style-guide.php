52c59c080ee99e602acc7c66031221e215094ddf
Alex Mueller <ajmueller6@gmail.com>
2015-01-09 14:57:29 -0800

    Adds map utility class and fixes control widths.
    Fixes #92.

--- Assets/src/templates/style-guide.php
+++ Assets/src/templates/style-guide.php
@@ -360,7 +360,7 @@
 			</div>
 			<div class="six columns">
 				<h2>Map - 4:3</h2>
-				<div class="aspect-ratio-4-3">
+				<div class="map">
 					<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1397.4432998107925!2d-122.69317991642879!3d45.532487514497674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sisite+design!5e0!3m2!1sen!2sus!4v1391532768363" frameborder="0" style="border:0"></iframe>
 				</div>
 			</div>
