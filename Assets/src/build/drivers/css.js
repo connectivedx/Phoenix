e42a92e6537b809d42612b455df6667a34695fa4
Alex Mueller <ajmueller6@gmail.com>
2015-01-19 17:31:18 -0800

    Adds CSS source maps to debugging builds.

--- Assets/src/build/drivers/css.js
+++ Assets/src/build/drivers/css.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var prefix = require('gulp-autoprefixer');
 var cssmin = require('gulp-minify-css');
 var cmq = require('gulp-combine-media-queries');
@@ -10,9 +11,11 @@ var gulpif = require('gulp-if');
 var CssDriver = {
 	build: function(pipeline, debug) {
 		return pipeline
+			.pipe(gulpif(debug, sourcemaps.init()))
 			.pipe(sass())
-			.pipe(prefix('last 2 versions', 'IE >= 8', 'Android >= 4'))
-			.pipe(cmq())
+			.pipe(gulpif(debug, sourcemaps.write('./')))
+			.pipe(gulpif(!debug, prefix('last 2 versions', 'IE >= 8', 'Android >= 4')))
+			.pipe(gulpif(!debug, cmq()))
 			.pipe(gulpif(!debug, cssmin({
 				noAdvanced: true
 			})));
