213754656cbf2c830f2f706d8a7ff2b1fcaeeca5
Christoph Saxe <christoph.saxe@gmail.com>
2015-03-12 09:28:34 -0700

    Adds ignore path to cache-busting (gulp-rev-all) for images in /img/content

--- Assets/src/build/tasks.js
+++ Assets/src/build/tasks.js
@@ -37,8 +37,10 @@ module.exports = function(configuration) {
 
 		var masterStream = sLoader.getTaskStreams();
 
-		if (configuration.cacheBusting) { // rev stamps all files with a hash for cache-busting
-			masterStream = masterStream.pipe(rev({ ignore: ['.php'] }));
+		if (configuration.cacheBusting) {
+			// rev stamps all files with a hash for cache-busting
+			// ignores php files and contents of /img/content directory.
+			masterStream = masterStream.pipe(rev({ ignore: ['.php', /img\/content/] }));
 		}
 
 		masterStream = sLoader.executeCustomOutput(masterStream);
