766cd925466be530d0bf6c71c316c4afae121285
dolsen <dolsen@connectivedx.com>
2015-04-10 14:38:49 -0700

    Adding sprite SCSS handlebars template for mapping

--- Assets/src/build/drivers/sprite.js
+++ Assets/src/build/drivers/sprite.js
@@ -1,25 +1,27 @@
 /*global require, module, process */
+//algorithm types = top-down | left-right | diagonal | alt-diagonal | binary-tree (default)
+
 'use strict';
 var gulp = require('gulp');
 var spritesmith = require('gulp.spritesmith');
 
 var SpriteDrive = {
 	build: function(pipeline, debug) {
-		var sprite = pipeline.pipe(
-						spritesmith({
-							algorithm: 'alt-diagonal',
-							cssName: '_sprites.scss',
-							cssOpts: {
-								functions: false
-							},
-							imgName: 'compiled-sprite.png',
-							imgPath: '#{$imagesFolder}compiled-sprite.png'
-						})
-					);
-		sprite.img.pipe(gulp.dest('img/css'));
-		sprite.css.pipe(gulp.dest('css/variables'));
-		return pipeline;			
+		var sprites = pipeline.pipe(
+			spritesmith({
+				//algorithm: 'binary-tree', 
+				cssTemplate: './build/lib/sprite.handlebars', //SCSS variables template
+				padding: 5, //Spacing between sprite images
+				cssSpritesheetName: 'main-sheet',
+				cssName: '_sprites.scss',
+				imgName: 'main-sprite.png',
+				imgPath: '#{$imagesFolder}main-sprite.png'
+			})			
+		);
+		sprites.img.pipe(gulp.dest('img/css'));
+		sprites.css.pipe(gulp.dest('css/variables/tmp'));
+
+		return pipeline;
 	}
 };
-
 module.exports = SpriteDrive;
