2ef28718d61687e4c464480ec28bf8371ad7979f
Alex Mueller <ajmueller6@gmail.com>
2015-06-11 19:50:29 -0700

    Adds browser-alert class for older browsers.

--- Assets/src/templates/inc/head.php
+++ Assets/src/templates/inc/head.php
@@ -28,6 +28,11 @@
 </head>
 
 <body class="<?php echo $bodyClass; ?>">
+	<!--[if lte IE 8]>
+		<div class="browser-alert">
+			You&rsquo;re using an older web browser version that may make your computer unsafe. <a href="http://browsehappy.com/">Download the latest version</a> to keep your information safe and improve your experience.
+		</div>
+	<![endif]-->
 	<div class="row">
 		<div class="eight columns centered" style="border: none;">
 			<a href="index.php">
